29cb1071800bf746ca5d8e8b11d385ad04970ee0
chamingyeong <165614132+chamingyeong00@users.noreply.github.com>
2025-09-12 19:04:04 +0900

    Update MealTicketSystemWasApplicationTests.java

--- src/test/java/yuseteam/mealticketsystemwas/MealTicketSystemWasApplicationTests.java
+++ src/test/java/yuseteam/mealticketsystemwas/MealTicketSystemWasApplicationTests.java
@@ -3,7 +3,7 @@ package yuseteam.mealticketsystemwas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class MealTicketSystemWasApplicationTests {
 
     @Test
