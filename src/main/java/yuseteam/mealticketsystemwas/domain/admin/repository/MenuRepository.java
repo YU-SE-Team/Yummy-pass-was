9ae22a5fa7af75ee01a851f535acbc745f0468cc
silverblond <kdg23538@yu.ac.kr>
2025-09-18 14:45:01 +0900

    [feat/#13]-add-find-function-in-repo

--- src/main/java/yuseteam/mealticketsystemwas/domain/admin/repository/MenuRepository.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/admin/repository/MenuRepository.java
@@ -3,5 +3,9 @@ package yuseteam.mealticketsystemwas.domain.admin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yuseteam.mealticketsystemwas.domain.admin.entity.Menu;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu,Long> {
+
+    List<Menu> findByRestaurantIdAndCategory(Long restaurantId, String category);
 }
