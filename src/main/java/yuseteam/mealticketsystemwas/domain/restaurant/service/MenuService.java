02a836bdfeacfb490cb180eb7b889c60356013ce
silverblond <kdg23538@yu.ac.kr>
2025-09-18 15:09:51 +0900

    [feat/#13]-create-menu-service

---
+++ src/main/java/yuseteam/mealticketsystemwas/domain/restaurant/service/MenuService.java
@@ -0,0 +1,25 @@
+package yuseteam.mealticketsystemwas.domain.restaurant.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import yuseteam.mealticketsystemwas.domain.admin.entity.Menu;
+import yuseteam.mealticketsystemwas.domain.admin.repository.MenuRepository;
+import yuseteam.mealticketsystemwas.domain.restaurant.dto.MenuResponseDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    @Transactional(readOnly = true)
+    public List<MenuResponseDto> getMenuByRestaurantAndCategory(Long restaurantId, String category) {
+        return menuRepository.findByRestaurantIdAndCategory(restaurantId, category).stream()
+                .map(MenuResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
