71aace4806f2a00e1aad2a941eb2488d716ab0e1
silverblond <kdg23538@yu.ac.kr>
2025-10-01 02:35:40 +0900

    [feat/#16-refactor-qrCreate-controller2service]

--- src/main/java/yuseteam/mealticketsystemwas/domain/qr/service/QrService.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/qr/service/QrService.java
@@ -1,15 +1,49 @@
 package yuseteam.mealticketsystemwas.domain.qr.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.security.access.AccessDeniedException;
 import yuseteam.mealticketsystemwas.config.SecurityUtil;
+import yuseteam.mealticketsystemwas.domain.qr.dto.QrCreateResponse;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class QrService {
 
     private final S3Service s3Service;
+
+    //ticket쪽에 생성하기 위한 메소드가 필요해서 controller->service단으로 이동
+    public QrCreateResponse createAndUploadQr() {
+        try {
+            String uuid = UUID.randomUUID().toString();
+            String contents = uuid;
+
+            int width = 200;
+            int height = 200;
+            BitMatrix matrix = new MultiFormatWriter()
+                    .encode(contents, BarcodeFormat.QR_CODE, width, height);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(matrix, "PNG", baos);
+            byte[] pngBytes = baos.toByteArray();
+
+            String imgKey = s3Service.imageKey(uuid);
+            String imageUrl = s3Service.uploadImageBytes(pngBytes, imgKey, MediaType.IMAGE_PNG_VALUE);
+
+            return new QrCreateResponse(uuid, imageUrl);
+
+        } catch (Exception e) {
+            throw new RuntimeException("QR 생성 또는 업로드에 실패했습니다.", e);
+        }
+    }
 
     public void validateStudentRole() {
         String role = SecurityUtil.getCurrentUserRole();
