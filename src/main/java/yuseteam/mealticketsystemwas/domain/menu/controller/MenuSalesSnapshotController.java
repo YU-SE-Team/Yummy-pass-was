8f086dcc463a4b927d6b13d64eb05d5e6595e755
silverblond <kdg23538@yu.ac.kr>
2025-10-06 01:38:12 +0900

    [chore/#113-add-get-today-sales-graph-api-controller]

--- src/main/java/yuseteam/mealticketsystemwas/domain/menu/controller/MenuSalesSnapshotController.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/menu/controller/MenuSalesSnapshotController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yuseteam.mealticketsystemwas.domain.menu.dto.MenuSalesGraphRes;
@@ -11,6 +12,7 @@ import yuseteam.mealticketsystemwas.domain.menu.entity.Menu;
 import yuseteam.mealticketsystemwas.domain.menu.entity.MenuSalesSnapshot;
 import yuseteam.mealticketsystemwas.domain.menu.repository.MenuRepository;
 import yuseteam.mealticketsystemwas.domain.menu.repository.MenuSalesSnapshotrepository;
+import yuseteam.mealticketsystemwas.domain.menu.service.MenuSalesSnapshotService;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -25,6 +27,7 @@ public class MenuSalesSnapshotController {
 
     private final MenuSalesSnapshotrepository menuSalesSnapshotrepository;
     private final MenuRepository menuRepository;
+    private final MenuSalesSnapshotService menuSalesSnapshotService;
 
     @GetMapping("/sales-diff")
     public ResponseEntity<Map<Long, Integer>> getLatestIntervalSales() {
@@ -39,5 +42,11 @@ public class MenuSalesSnapshotController {
             res.put(menu.getId(), latestSales);
         }
         return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/{menuId}/today-sales-graph")
+    public ResponseEntity<MenuSalesGraphRes> getTodaySales(@PathVariable Long menuId) {
+        MenuSalesGraphRes response = menuSalesSnapshotService.getMenuSalesSnapshot(menuId);
+        return ResponseEntity.ok(response);
     }
 }
