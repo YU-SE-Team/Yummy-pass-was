6ac1aa26f76c6a10f79d33ac380d86b5439d5c40
silverblond <kdg23538@yu.ac.kr>
2025-10-07 21:32:35 +0900

    [chore/#130-add-dto-swagger]

--- src/main/java/yuseteam/mealticketsystemwas/domain/menu/common/dto/MenuDetailResponse.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/menu/common/dto/MenuDetailResponse.java
@@ -1,13 +1,22 @@
 package yuseteam.mealticketsystemwas.domain.menu.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import yuseteam.mealticketsystemwas.domain.menu.common.entity.Menu;
 
 @Getter
+@Schema(description = "메뉴 상세 정보 응답")
 public class MenuDetailResponse {
+    @Schema(description = "메뉴 ID", example = "1")
     private final Long id;
+
+    @Schema(description = "메뉴 이름", example = "치킨마요덮밥")
     private final String name;
+
+    @Schema(description = "메뉴 가격", example = "8000")
     private final int price;
+
+    @Schema(description = "남은 재고 수량 (총 재고량 - 판매량)", example = "45")
     private final int remainingTickets;
 
     public MenuDetailResponse(Menu menu) {
--- src/main/java/yuseteam/mealticketsystemwas/domain/menu/common/dto/MenuResponse.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/menu/common/dto/MenuResponse.java
@@ -1,13 +1,22 @@
 package yuseteam.mealticketsystemwas.domain.menu.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import yuseteam.mealticketsystemwas.domain.menu.common.entity.Menu;
 
 @Getter
+@Schema(description = "메뉴 목록 응답")
 public class MenuResponse {
+    @Schema(description = "메뉴 ID", example = "1")
     private final Long id;
+
+    @Schema(description = "메뉴 이름", example = "치킨마요덮밥")
     private final String name;
+
+    @Schema(description = "메뉴 사진 URL", example = "https://s3.amazonaws.com/bucket/menu1.jpg")
     private final String photoUrl;
+
+    @Schema(description = "메뉴 가격", example = "8000")
     private final int price;
 
     public MenuResponse(Menu menu) {
