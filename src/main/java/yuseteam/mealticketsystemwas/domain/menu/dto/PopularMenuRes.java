bda37cbe352a67d900fd5bdc40896816e5b1403f
silverblond <kdg23538@yu.ac.kr>
2025-10-07 16:05:46 +0900

    [feat/#74-add-popualrRes-dto]

--- src/main/java/yuseteam/mealticketsystemwas/domain/menu/dto/PopularMenuRes.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/menu/dto/PopularMenuRes.java
@@ -1,4 +1,11 @@
 package yuseteam.mealticketsystemwas.domain.menu.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class PopularMenuRes {
+    private String menuName;
+    private Integer totalSalesLast20Minutes;
 }
