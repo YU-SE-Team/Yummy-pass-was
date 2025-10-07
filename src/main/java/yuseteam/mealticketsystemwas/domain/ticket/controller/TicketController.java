9390f22e61349b80a0c2bc02992f01ceed485ab1
silverblond <kdg23538@yu.ac.kr>
2025-10-07 22:53:32 +0900

    [chore/#134-fix-ticket-title]

--- src/main/java/yuseteam/mealticketsystemwas/domain/ticket/controller/TicketController.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/ticket/controller/TicketController.java
@@ -1,5 +1,6 @@
 package yuseteam.mealticketsystemwas.domain.ticket.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/tickets")
+@Tag(name = "Ticket", description = "티켓 사용·미사용·만료 조회 API")
 public class TicketController {
     private final TicketService ticketService;
 
