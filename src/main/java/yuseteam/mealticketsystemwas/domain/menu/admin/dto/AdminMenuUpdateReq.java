a5eda797c395882bc20fa33080eb363b4d47d14d
subin <han04245@naver.com>
2025-11-22 00:14:02 +0900

    fix: edit menu visible 문제

--- src/main/java/yuseteam/mealticketsystemwas/domain/menu/admin/dto/AdminMenuUpdateReq.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/menu/admin/dto/AdminMenuUpdateReq.java
@@ -3,10 +3,12 @@ package yuseteam.mealticketsystemwas.domain.menu.admin.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 import yuseteam.mealticketsystemwas.domain.menu.common.entity.MenuCategory;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Schema(description = "관리자 메뉴 수정 요청 DTO")
 public class AdminMenuUpdateReq {
