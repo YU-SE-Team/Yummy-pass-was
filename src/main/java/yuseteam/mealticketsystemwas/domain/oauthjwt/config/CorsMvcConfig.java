03fcc8e90c1645fc6ad6b408e40101566292f9cb
subin <han04245@naver.com>
2025-12-06 21:29:45 +0900

    ref: cors,security 수정 전으로 복구

--- src/main/java/yuseteam/mealticketsystemwas/domain/oauthjwt/config/CorsMvcConfig.java
+++ src/main/java/yuseteam/mealticketsystemwas/domain/oauthjwt/config/CorsMvcConfig.java
@@ -1,36 +1,26 @@
 package yuseteam.mealticketsystemwas.domain.oauthjwt.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsMvcConfig {
+public class CorsMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
 
-        // 1. 허용할 프론트엔드 도메인들
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://localhost:8080",
-                "https://yummy-test-ashy.vercel.app",
-                "https://www.yummypass.r-e.kr",
-                "https://yummypass.n-e.kr"
-        ));
-
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8080",
+                        "https://yummy-test-ashy.vercel.app",
+                        "https://www.yummypass.r-e.kr",
+                        "https://yummypass.n-e.kr"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Set-Cookie", "Authorization");
     }
 }
